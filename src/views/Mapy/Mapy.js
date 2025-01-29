e0dbfe830cd86e9a490861cd8f8e391e928b1546
czpavsed <160134914+czpavsed@users.noreply.github.com>
2025-01-29 16:46:23 +0100

    Update Mapy.js

--- src/views/Mapy/Mapy.js
+++ src/views/Mapy/Mapy.js
@@ -37,7 +37,7 @@ const Mapy = () => {
 
       setLoading(true);
       try {
-        const response = await axios.get('https://portal.derator.cz/api/mapy', {
+        const response = await axios.get('/api/mapy', {
           params: { zakaznikId },
           headers: {
             'Authorization': `Bearer ${API_ACCESS_KEY}`,
